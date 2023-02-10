283fb9cfe46191875629f8eee4b5b98a52d21ee0
Pritish Mishra <pritishmishra579@gmail.com>
2023-02-10 18:15:44 +0530

    added target="_blank"

--- components/TimeBox.tsx
+++ components/TimeBox.tsx
@@ -9,7 +9,7 @@ export default function TimeBox({ text, linkText, link, timestamp }) {
                     {timestamp}
                 </time>
                 <div className="text-sm font-normal text-gray-500 lex dark:text-gray-300">{text}
-                    <a href={link} className="font-semibold text-gray-900 dark:text-white hover:underline underline-offset-4">
+                    <a href={link} target="_blank" rel="noopener noreferrer" className="font-semibold text-gray-900 dark:text-white hover:underline underline-offset-4">
                         {linkText}
                     </a>
                 </div>
