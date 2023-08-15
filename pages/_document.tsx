417c4d4313145430e12f216a95035152d3375bd1
Pritish Mishra <pritish.mishra@optym.com>
2023-08-15 16:02:10 +0530

    chore: :chart_with_upwards_trend: MS Clarity

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,30 +7,44 @@ export default function Document() {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/apple-touch-icon.png" />
+          href="/apple-touch-icon.png"
+        />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon-32x32.png" />
+          href="/favicon-32x32.png"
+        />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon-16x16.png" />
-        <link
-          rel="manifest"
-          href="/site.webmanifest" />
-        <meta
-          name="msapplication-TileColor"
-          content="#ffffff" />
-        <meta
-          name="theme-color"
-          content="#ffffff" />
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
         <meta
           content="max-snippet:-1, max-image-preview:large, max-video-preview:-1"
-          name="robots" />
-        <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "abf38da9bf214a54b7550cee4b415c7b"}'></script>
+          name="robots"
+        />
+        <script
+          defer
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          data-cf-beacon='{"token": "abf38da9bf214a54b7550cee4b415c7b"}'
+        ></script>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "ifkdxdy836");
+            `
+          }}
+        />
       </Head>
       <body className="text-white bg-white dark:bg-black dark:text-black">
         <Main />
