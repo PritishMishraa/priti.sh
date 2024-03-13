3d37719b14cef953aa4bd30ad253ca35708878b7
Pritish Mishra <pritishmishra579@gmail.com>
2024-03-13 23:47:00 +0530

    refactor: :recycle: Add book extraction functions and refactor code

--- pages/api/goodreads.ts
+++ pages/api/goodreads.ts
@@ -3,20 +3,76 @@ import Parser from 'rss-parser';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 const parser = new Parser();
+interface Book {
+    title?: string;
+    link?: string;
+    pubDate?: string;
+    content?: string;
+    contentSnippet?: string;
+    guid?: string;
+    isoDate?: string;
+}
+interface BookDetails {
+    bookName: string | null;
+    authorName: string | null;
+}
+
+function extractNumberBeforePercentage(title: string): number | null {
+    const match = title.match(/(\d+)%/);
+    if (match) {
+        return parseInt(match[1]);
+    }
+    return null;
+}
+
+function extractBookNameFromTitle(title: string): string | null {
+    const match = title.match(/with\s+(.*?)\n/);
+    if (match && match.length >= 2) {
+        return match[1].trim();
+    }
+    return null;
+}
+
+function extractAuthorNameFromContent(content: string): BookDetails {
+    const bookDetails: BookDetails = {
+        bookName: null,
+        authorName: null
+    };
+
+    const authorNameMatch = content.match(/alt="[^"]*\sby\s([^"]+)"/);
+    if (authorNameMatch && authorNameMatch.length >= 2) {
+        bookDetails.authorName = authorNameMatch[1].trim();
+    }
+
+    return bookDetails;
+}
+
+function findObjectWithTitleContainingPercentage(data: Book[]): { percentageRead: number | null, bookDetails: BookDetails } {
+    for (let i = 0; i < data.length; i++) {
+        const title = data[i].title;
+        if (title.includes('%')) {
+            const numberBeforePercentage = extractNumberBeforePercentage(title);
+            const bookName = extractBookNameFromTitle(title);
+
+            if (numberBeforePercentage !== null) {
+                const bookDetails = extractAuthorNameFromContent(data[i].content || '');
+                return {
+                    percentageRead: numberBeforePercentage,
+                    bookDetails: {bookName: bookName, authorName: bookDetails.authorName}
+                };
+            }
+        }
+    }
+    return { percentageRead: null, bookDetails: { bookName: null, authorName: null } };
+}
 
 export default async function handler(_req: NextApiRequest, res: NextApiResponse) {
     const feed = await parser.parseURL('https://www.goodreads.com/user/updates_rss/124720208');
 
-    const bookNameByAuthName = feed.items[0].content.match(/\w+|"[^"]+"/g)[9].slice(1, -1);
-    const pages = feed.items[0].title.replace(/^\s+|\s+$/gm, '').split(" ");
+    const { percentageRead, bookDetails } = findObjectWithTitleContainingPercentage(feed.items);
 
-    const bookNameAndAuthNameArray = bookNameByAuthName.split(" by ");
-    const bookName = bookNameAndAuthNameArray[0];
-    const authName = bookNameAndAuthNameArray[1];
-
-    const pageAt = Number(pages[3]);
-    const totalPages = Number(pages[5]);
-    const percentageRead = ((pageAt * 100) / totalPages);
+    const bookName = bookDetails.bookName;
+    const authName = bookDetails.authorName;
 
     res.setHeader(
         'Cache-Control',
