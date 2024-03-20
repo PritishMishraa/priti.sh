cb12ac332240d6c8c362d038705e88fdf33d2c6d
Pritish Mishra <pritishmishra579@gmail.com>
2024-03-20 17:29:22 +0530

    fix: :ambulance: Fix link in MobileMenu component

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -72,7 +72,7 @@ export default function MobileMenu() {
             className="text-sm font-semibold text-gray-900 border-b border-gray-300 dark:border-gray-700 dark:text-gray-100"
             style={{ transitionDelay: '175ms' }}
           >
-            <Link href="/projects" className="flex w-auto pb-4">
+            <Link href="/project" className="flex w-auto pb-4">
               Projects
             </Link>
           </li>
