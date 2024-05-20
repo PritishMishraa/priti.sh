ab53767eb04f0539f12824d7855d2e8f9813596c
Pritish Mishra <pritishmishra579@gmail.com>
2024-05-20 12:51:53 +0530

    refactor: Reactify  SVG attributes

--- components/Container.tsx
+++ components/Container.tsx
@@ -85,9 +85,9 @@ export default function Container(props) {
                     role="img"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round">
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round">
                     <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" /><path d="M14 2v4a2 2 0 0 0 2 2h4" /><path d="M10 9H8" /><path d="M16 13H8" /><path d="M16 17H8" />
                   </svg>
                 </a>
