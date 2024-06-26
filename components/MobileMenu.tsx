de0992162cfbc49ca757aeb376f4e31adcd5beb0
Pritish Mishra <pritishmishra579@gmail.com>
2024-06-26 22:14:41 +0530

    chore: reorder

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -1,8 +1,8 @@
 import cn from 'classnames';
 import Link from 'next/link';
-import useDelayedRender from 'lib/use-delayed-render';
 import { useState, useEffect } from 'react';
 import styles from 'styles/mobile-menu.module.css';
+import useDelayedRender from 'lib/use-delayed-render';
 
 const ExternalLink = ({ href, children }) => (
   <a
